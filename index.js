c7ed33e89011c4ab7294dac4bff0825db117542e
acvan11 <anvanc37@gmail.com>
2019-03-30 10:47:37 -0700

    set up the PORT for heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express(); // app object sets up configuration
 //that will listen for incoming request
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ hi: there });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000; // for heroku deployment
+//if it has a port on heroku, run for that port, otherwise, the port is 5000
+app.listen(PORT);
